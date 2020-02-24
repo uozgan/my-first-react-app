14b3ab9b3bd8da27bfd1ca2296aea08ff4b69c79
uozgan <utkuozgan_8@hotmail.com>
2020-02-24 12:06:49 +0100

    React Intro

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Intro</h1>
+      
+     <p>In this section of lesson we are goning to learn React!</p>
     </div>
   );
 }
